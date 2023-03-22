7884c165330f412c22dc2b5f3d4ee767abffff8d
tuancan <tuacandongsang@gmail.com>
2023-03-23 00:54:22 +0700

    a

--- frontend/src/layout/layout.vue
+++ frontend/src/layout/layout.vue
@@ -3,8 +3,6 @@
     <Header />
     <router-view></router-view>
     <Footer />
-    <!-- <a-back-top />
-    <strong style="color: rgba(0, 176, 78, 0.6)"></strong> -->
     <BackToTop />
   </div>
 </template>
@@ -12,7 +10,7 @@
 <script>
 import Header from "./component/header/Header.vue";
 import Footer from "./component/footer/Footer.vue";
-import BackToTop from "../components/backtotop/BackToTop.vue";
+import BackToTop from "@/components/backtotop/BackToTop.vue";
 export default {
   components: { Header, Footer, BackToTop },
 };
