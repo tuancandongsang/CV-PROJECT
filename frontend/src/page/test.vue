d6c44eed35a1a442b968666c95598e5a26b11ef5
tuancandongsang <tuancandongsang@gmail.com>
2023-03-23 17:51:01 +0700

    a

--- frontend/src/page/test.vue
+++ frontend/src/page/test.vue
@@ -1,154 +1,142 @@
 <template>
-  <div class="wrapper">
-    <TransitionGroup
-      id="list"
-      name="swipe"
-      tag="div"
-      class="transition-container"
-    >
-      <img
-        v-for="(picture, i) in pictures"
-        :key="picture.src"
-        :src="picture.src"
-        alt=""
-        class="image"
-      />
-    </TransitionGroup>
-    <button class="previous" @click="previous">
-      <svg
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1792 1792"
-        style="enable-background: new 0 0 1792 1792"
-        xml:space="preserve"
-      >
-        <path
-          d="M259.9,983.3c-23.6-23.2-35.9-55.6-33.6-88.6c-2.4-31.9,10-63,33.6-84.6l546.3-548.3c45.4-45.4,119-45.4,164.4,0
-	s45.4,119,0,164.4L616.3,780.5h833.6c63.8-0.4,115.8,51,116.1,114.8c0,0.2,0,0.4,0,0.7c-0.4,64-52.1,115.8-116.1,116.1H616.3
-	l354.4,353.7c45.4,45.4,45.4,119,0,164.4s-119,45.4-164.4,0l0,0L259.9,983.3z"
-        />
-      </svg>
-    </button>
-    <button class="next" @click="next">
-      <svg
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1792 1792"
-        style="enable-background: new 0 0 1792 1792"
-        xml:space="preserve"
-      >
-        <path
-          d="M1532.1,808.7c23.6,23.2,35.9,55.6,33.6,88.6c2.4,31.9-10,63-33.6,84.6l-546.3,548.3c-45.4,45.4-119,45.4-164.4,0
-	c-45.4-45.4-45.4-119,0-164.4l354.4-354.4H342.1c-63.8,0.4-115.8-51-116.1-114.8c0-0.2,0-0.4,0-0.7c0.4-64,52.1-115.8,116.1-116.1
-	h833.6L821.4,426.2c-45.4-45.4-45.4-119,0-164.4c45.4-45.4,119-45.4,164.4,0l0,0L1532.1,808.7z"
-        />
-      </svg>
-    </button>
+  <div id="slider">
+    <div class="slider">
+      <ul class="slides" :style="{ left: -width * current + 'px' }">
+        <li v-for="(slide, i) in slides" :key="i">
+          <img :src="slide" alt="" />
+        </li>
+      </ul>
+      <ul class="bullets">
+        <li
+          v-for="(slide, i) in slides"
+          :key="i"
+          v-html="i == current ? '&#9679;' : '&omicron;'"
+        ></li>
+      </ul>
+    </div>
+    <a class="prev" href="#" @click.prevent="prevSlide">&#x25C0;</a>
+    <a class="next" href="#" @click.prevent="nextSlide">&#x25B6;</a>
   </div>
 </template>
 <script>
-import { defineComponent, ref } from "vue";
-export default defineComponent({
-  setup() {
-    const pictures = ref(
-      Array.from({ length: 10 }, (_, i) => ({
-        src: `https://picsum.photos/600/400?random=${i + 1}`,
-      }))
-    );
-
-    const previous = () => {
-      const lastSlide = pictures.value.pop();
-      pictures.value = [lastSlide].concat(pictures.value);
-    };
-
-    const next = () => {
-      const firstPicture = pictures.value.shift();
-      pictures.value = pictures.value.concat(firstPicture);
-    };
+export default {
+  data() {
     return {
-      pictures,
-      previous,
-      next,
+      slides: [
+        "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg",
+        "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg",
+        "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg",
+        "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg",
+        "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg",
+      ],
+      current: 0,
+      width: 800,
+      timer: 0,
     };
   },
-});
+  methods: {
+    nextSlide: function () {
+      this.current++;
+      if (this.current >= this.slides.length) this.current = 0;
+      this.resetPlay();
+    },
+    prevSlide: function () {
+      this.current--;
+      if (this.current < 0) this.current = this.slides.length - 1;
+      this.resetPlay();
+    },
+    selectSlide: function (i) {
+      this.current = i;
+      this.resetPlay();
+    },
+    resetPlay: function () {
+      clearInterval(this.timer);
+      this.play();
+    },
+    play: function () {
+      let app = this;
+      this.timer = setInterval(function () {
+        app.nextSlide();
+      }, 2000);
+    },
+  },
+  created: function () {
+    this.play();
+  },
+};
 </script>
 
-<style lang="postcss">
-.wrapper {
-  width: 100vw;
-  height: 100vh;
-  display: grid;
-  place-content: center;
-}
-
-.transition-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+<style lang="scss" scoped>
+#slider {
   width: 100%;
+  height: 500px;
+  position: relative;
+  .prev,
+  .next {
+    text-decoration: none;
+    color: green;
+    // padding: 50px 60px;
+    // background-color: red;
+    border-radius: 50%;
+    position: absolute;
+    z-index: 1;
+    font-size: 42px;
+    top: 50%;
+    transform: translate(0, -50%);
+    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+  }
+  .prev {
+    left: 20px;
+  }
+  .next {
+    right: 20px;
+  }
+}
+
+.slider {
+  margin: 0 auto;
+  padding: 0;
+  width: 1150px;
   height: 100%;
-  overflow: visible;
-}
-
-.image {
-  width: 40vw;
-  z-index: 10;
-  pointer-events: none;
-}
-.image:first-child {
-  z-index: 5;
-}
-
-.image:last-child {
-  z-index: 5;
-}
-
-.previous,
-.next {
-  position: absolute;
-  top: 50%;
-  z-index: 15;
-  width: 48px;
-  height: 48px;
-  padding: 12px;
-  border-radius: 50%;
-  border: none;
-  background: white;
-  cursor: pointer;
-  opacity: 0.5;
-  transition: opacity 0.3s ease;
-}
-
-.previous {
-  left: 20px;
-}
-
-.next {
-  right: 20px;
-}
-
-.previous:hover,
-.next:hover {
-  opacity: 1;
-}
-
-.previous > svg,
-.next > svg {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
-.swipe-move {
-  transition: all 0.3s;
+  position: relative;
+  overflow: hidden;
+  ul.slides {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    display: table;
+    position: absolute;
+    top: 0;
+    transition: left 800ms;
+    li {
+      list-style-type: none;
+      display: table-cell;
+      width: 100%;
+      img {
+        width: 1150px;
+        object-fit: fill;
+      }
+    }
+  }
+  ul.bullets {
+    width: inherit;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 0;
+    margin: 0 0 10px 0;
+    text-align: center;
+    z-index: 1;
+    li {
+      list-style-type: none;
+      display: inline;
+      color: #fff;
+      cursor: pointer;
+      padding: 0 5px;
+      font-size: 20px;
+      font-family: sans-serif;
+    }
+  }
 }
 </style>
---
+++ frontend/src/page/test2.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="example">
+    <carousel-3d
+      :autoplay="true"
+      :autoplay-timeout="3000"
+      :controls-visible="true"
+      :controls-prev-html="'&#x2039; '"
+      :controls-next-html="'&#x203A;'"
+      :controls-width="10"
+      :controls-height="30"
+      :clickable="false"
+    >
+      <slide :index="0">
+        <img
+          src="https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+        />
+      </slide>
+
+      <slide :index="1">
+        <img
+          src="https://thienha3d.vn/wp-content/uploads/2019/01/hinh-dan-tuong-phong-canh-04.jpg"
+        />
+      </slide>
+
+      <slide :index="2">
+        <img
+          src="https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+        />
+      </slide>
+
+      <slide :index="3">
+        <img src="https://zshop.vn/images/link/63/1045.jpg?t=1474958283" />
+      </slide>
+
+      <slide :index="4">
+        <img
+          src="https://kenh14cdn.com/thumb_w/660/2019/3/21/photo-1-1553155372658221231913.jpg"
+        />
+      </slide>
+
+      <slide :index="5">
+        <img
+          src="http://product.hstatic.net/1000298822/product/hinh-anh-dep-ve-tinh-yeu-chung-thuy_95129557953643089a963335468f3ec0.jpg"
+        />
+      </slide>
+
+      <slide :index="6">
+        <img
+          src="https://i.pinimg.com/originals/53/b0/77/53b077524c0dd34ac0cf92631ab1e9a6.jpg"
+        />
+      </slide>
+    </carousel-3d>
+  </div>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+export default {
+  components: {
+    Carousel3d,
+    Slide
+  }
+};
+</script>
+
+<style scoped>
+.carousel-3d-slide {
+  height: unset !important;
+}
+
+.carousel-3d-slide {
+  box-shadow: 0 6px 6px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+}
+.prev[data-v-05517ad0] {
+  left: 244px !important;
+  top: 0px !important;
+}
+.next[data-v-05517ad0] {
+  right: 225px !important;
+  top: 0px !important;
+}
+.Next slide span {
+  font-size: 20px;
+}
+.carousel-3d-container .next span {
+  font-size: 40px;
+}
+.carousel-3d-container .prev span {
+  font-size: 40px;
+}
+.carousel-3d-slide {
+  width: 400px !important;
+}
+</style>
